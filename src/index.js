87d74f97240b8a3077104ae255c64ddfa9e70c53
Nadav Lachish <d_lahish@walla.com>
2016-08-25 14:56:47 +0200

    work on adding font aweome

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import Root from './containers/Root'
+require('font-awesome/css/font-awesome.css')
 
 render(
     <Root />, document.getElementById('app')
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,33 @@ module.exports = {
       test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
       loader: "file"
       },
-      { test: /\.json$/, loader: "json-loader" }
+      { test: /\.json$/, loader: "json-loader" },
+      {
+      test: /\.css$/,
+      // loader: 'style!css?sourceMap'
+      loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/octet-stream"
+      },
+      {
+        // test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/,
+        loader: "file"
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=image/svg+xml"
+      }
     ]
     },
     resolve: {
